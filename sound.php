c1076a64211a3364dda4642488a0ad3dab16c6ba
DarrenYang <telunyang@gmail.com>
2021-07-21 01:09:56 +0800

    更新 google 小姐範例

--- sound.php
+++ sound.php
@@ -18,28 +18,32 @@ if( isset($_POST['q']) && $_POST['q'] !== '' ){
     //請求網址
     $url = "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=zh-TW&q=".urlencode($_POST['q']);
     
-    //模擬網路請求，取得實際聲音資料
-    $ch = curl_init(); //curl 初始化
-    curl_setopt($ch, CURLOPT_URL, $url); //設定 URL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //若有轉址，一路轉到正常顯示的頁面
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); //指定 Request Headers
-    $raw_data = curl_exec($ch); //取得聲音內容 (可能是亂碼)
-    curl_close($ch); //關閉 curl
-    
-    //以 q 的傳遞資料，整合 md5 作為聲音檔案命名
-    $fileName = hash('md5', $url);
-    
-    //整合路徑
-    $file_path = "tmp/{$fileName}.mp3";
-    
-    //建立聲音檔
-    if( $fp = fopen($file_path, "w") ){
-        fwrite($fp, $raw_data);
-        fclose($fp);
-    
-        $obj['success'] = true;
-        $obj['info'] = '下載成功';
-        $obj['link'] = "https://darreninfo.cc/tts/{$file_path}";
+    try {
+        //模擬網路請求，取得實際聲音資料
+        $ch = curl_init(); //curl 初始化
+        curl_setopt($ch, CURLOPT_URL, $url); //設定 URL
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); //若有轉址，一路轉到正常顯示的頁面
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); //指定 Request Headers
+        $raw_data = curl_exec($ch); //取得聲音內容 (可能是亂碼)
+        curl_close($ch); //關閉 curl
+        
+        //以 q 的傳遞資料，整合 md5 作為聲音檔案命名
+        $fileName = hash('md5', $url);
+        
+        //整合路徑
+        $file_path = "tmp/{$fileName}.mp3";
+        
+        //建立聲音檔
+        if( $fp = fopen($file_path, "w") ){
+            fwrite($fp, $raw_data);
+            fclose($fp);
+        
+            $obj['success'] = true;
+            $obj['info'] = '下載成功';
+            $obj['link'] = "https://localhost/php_iii_uiux-master/{$file_path}";
+        }
+    } catch(Exception $e){
+        $obj['info'] = "錯誤訊息: {$e->getMessage()}";
     }
 }
 
